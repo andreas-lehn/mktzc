9bba6ec0acb8aa4f1ee3ae1c27c41e0e0635703b
Andreas Lehn <andreas.lehn@icloud.com>
2020-03-17 15:16:35 +0100

    Possiblity to excludes files or directories added.

--- deb2tcz.sh
+++ deb2tcz.sh
@@ -11,7 +11,39 @@
 # Given a debian package file `libunwind8_1.2.1-9_armhf.deb`.
 # Then name of the extension will be `libunwind8`
 
+usage() {
+    echo "usage: $0 [-h] [-e <file>] <deb-package>"
+}
+
+help() {
+    usage
+    echo "Make TinyCoreLinux extension form debian package <deb-package>."
+    echo
+    echo "  -h           Show this help message."
+    echo "  -e <file>    Exclude file or directory <file> from TinyCoreLinux extension."
+    echo "               Option -e can be specified multiple times."
+}
+
+excludes=""
+
+while getopts e:h opt
+do
+    case $opt in
+        e) excludes="$excludes $OPTARG" ;;
+        h) help; exit 0 ;;
+        ?) usage; exit 1 ;;
+    esac
+done
+shift $((OPTIND-1))
+
 debfile=$1
+if [ -z "$debfile" ]
+then
+    echo "$0: Debian package missing."
+    usage
+    exit 1
+fi
+
 extension=$(echo $debfile | awk -F _ '{ print $1}')
 tarballname=$(ar -t $debfile | grep data.tar)
 
@@ -32,13 +64,22 @@ filesdir=$tempdir/$extension
 mkdir $filesdir
 tar xf $tarball -C $filesdir
 
+# remove files in excludes
+for f in $excludes
+do
+    if [ -e "$filesdir/$f" ]
+    then
+        rm -rf "$filesdir/$f"
+    else
+        echo "$0: $f: no such file or directory"
+    fi
+done
+
 # generate the file list of the extension
+rm -f $extension.tcz.list
 for f in $(cd $filesdir; find *)
 do
-    if [ ! -d $filesdir/$f ]
-    then
-        echo $f >> $extension.tcz.list 
-    fi
+    [ ! -d "$filesdir/$f" ] && echo $f >> $extension.tcz.list 
 done
 
 # squash the files together as an TinyCoreLinx extension
